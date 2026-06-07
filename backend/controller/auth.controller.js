dc9a3d2fb6e60e8c04b20852f079ad7858bc6ddb
apik-bardewa <apikbardewa77@gmail.com>
2026-06-07 20:58:27 +0545

    profile page ui is added

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -34,7 +34,7 @@ export const signup= async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             mazAge:7*24*60*60*1000,
-            sameSite:"None",
+            sameSite:"Strict",
             secure:false
         })
         return res.status(201).json(user);
@@ -67,7 +67,7 @@ export const login= async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             mazAge:7*24*60*60*1000,
-            sameSite:"None",
+            sameSite:"Strict",
             secure:false
         })
         return res.status(200).json(user);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import Nav from './pages/Nav'
-import Hero from './pages/Hero'
+// import Nav from './pages/Nav'
+// import Hero from './pages/Hero'
 import { Routes,Route } from 'react-router-dom'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Signout from './pages/Signout'
-
+import Profile from './pages/Profile'
+// import getCurrentUser from './pages/customHooks/getCurrentUser'
 
 function App() {
+  // getCurrentUser();
   return (
-    
     <div>
         <Routes>
           <Route path='/signin' element={<Signin/>}/>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/signout' element={<Signout/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+         
         </Routes>
     </div>
   )
